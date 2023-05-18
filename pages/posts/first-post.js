f4b1ec0de9b4b73a7d2a75f246673e976b7954f9
Ry <OfficiallyRy@outlook.com>
2023-05-18 15:13:33 -0600

    You can have different heads for different pages! My god!

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,13 @@
 import Link from "next/link.js";
 import React from "react";
+import Head from "next/head.js";
 
 export default function FirstPost() {
   return (
     <>
+    <Head>
+      <title>First Post</title>
+    </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
