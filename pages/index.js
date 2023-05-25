16b8fd36bd4b9d4b3f29bdd02b78945fcc6a66b3
Ry <OfficiallyRy@outlook.com>
2023-05-25 13:56:57 -0600

    Added function to get parsed posts data and index function to get that data as props

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from "../lib/posts.js";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
 
 export default function Home() {
   return (
